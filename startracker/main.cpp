d3c1e99f77b896e40e44b515c94014969844bad0
The Invisible Man <granados.carlos91@gmail.com>
2016-04-02 16:44:53 -0400

    Initial Commit

---
+++ startracker/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  startracker
+//
+//  Created by Carlos on 4/2/16.
+//  Copyright © 2016 Carlos Granados. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
